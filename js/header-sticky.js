a0e3a6b97c3cdfd58b4582f64dcf2f8d045ec7c0
panjihanum <48155803+panjihanum@users.noreply.github.com>
2019-08-30 02:20:59 +0700

    Middle Text

--- js/header-sticky.js
+++ js/header-sticky.js
@@ -3,14 +3,15 @@ window.addEventListener("scroll", function() {
   let opacityNav = $("#sticky");
   let custommenu = $(".custom-menu-hide");
 
-  if (window.pageYOffset > 0) {
+
+  if (window.pageYOffset > 0 || !$(window).scrollTop(0)) {
     custommenu.hide();
     opacityNav = opacityNav.find('.about-us-image');
     opacityNav.addClass("header-background-opacity");
     mainNav = mainNav.find(".header-menu");
     mainNav.addClass("is-sticky");
     mainNav.find('.logo-header-2').hide();
-  } else if($(window).scrollTop(0)) {
+  } else {
     custommenu.show();
     opacityNav = opacityNav.find('.about-us-image');
     opacityNav.removeClass("header-background-opacity");
